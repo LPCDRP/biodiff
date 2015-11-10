3ee5c4db995627475268dfc8dbc625437fef5540
Afif Elghraoui <aelghraoui@sdsu.edu>
2015-11-10 12:49:46 -0800

    Change the reference and the query in common.mk to match the reference ID

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
-REFERENCE ?= H37Ra_NC009525_1.fasta
+REFERENCE ?= H37Rv_NC000962_3.fasta
 REFERENCE_ID ?=gi|448814763|ref|NC_000962.3|
-QUERY ?= A6.fasta
+QUERY ?= BTB00042.fasta
 
 vpath %.fasta $(GROUPHOME)/data/genomes
 vpath %.fasta $(GROUPHOME)/resources
