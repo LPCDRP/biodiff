53fbd9f146579a9fc06bc66f27ed63432bcb6062
Afif Elghraoui <aelghraoui@sdsu.edu>
2016-04-11 12:18:47 -0700

    Remove unnecessary file and use proper name for configure.ac

--- common.mk
+++
@@ -1,6 +0,0 @@
-REFERENCE ?= H37Rv_NC000962_3.fasta
-REFERENCE_ID ?=gi|448814763|ref|NC_000962.3|
-QUERY ?= BTB00042.fasta
-
-vpath %.fasta $(GROUPHOME)/data/genomes
-vpath %.fasta $(GROUPHOME)/resources
