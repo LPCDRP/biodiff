5962982eb7253943b2ff96583c2632f23cd8e3a9
Derek Conkle-Gutierrez <dconkleg@valafar03.intern>
2018-05-24 13:36:01 -0700

    inversionfix.py now uses id from querry fasta for CHROM in vcf output

--- inversionfix.py
+++ inversionfix.py
@@ -17,30 +17,7 @@ def main():
     parser.add_argument('-t', '--tfasta', help='temp fasta file', default='out-temp.fasta')
     args = parser.parse_args()
 
-    #chromname = 'snps_indels|NC_001416.1|'
-    chromname = '1'
-    # parse args.rdiff and make args.vcf with just the inversions, translocations, etc. in vcf format (no header)
-    # which biodiff will eventually combine with its own vcf from the other differences it finds
-    vcflines = []
-    with open(args.rdiff) as rfile:
-        line2inv = False  # dnadiff records inversions in 2 lines. first occurrence is start, 2nd is end.
-        invstart = 0
-        for line in rfile:
-            cleanline = line.strip('\n')
-            rdiff = cleanline.split('\t')
-            # rdiff[2] is genome position, rdiff[1] is variation type (GAP, INV, etc)
-            if rdiff[1] == 'INV' and not line2inv:
-                vcfstring = chromname + '\t' + rdiff[2] + '\t.\tN\tINV\t.\tPASS\t'
-                line2inv = True
-                invstart = int(rdiff[2])
-            elif rdiff[1] == 'INV':
-                vcfstring += 'SVLEN=' + str(int(rdiff[2])-invstart) + ';SVTYPE=INV\n'
-                vcflines.append(vcfstring)
-                line2inv = False
-    with open(args.vcf, 'w') as vfile:
-        for line in vcflines:
-            vfile.write(line)
-
+    # Find inversions in terms of Querry position, and output temp.fasta with those inversions undone
     # parse args.qdiff to get inversions in querry coordinates
     # parse args.qfasta to get fasta of querry sequence,
     # undo the inversions found in args.qdiff
@@ -70,11 +47,34 @@ def main():
         after = qseq[invdict['End']:]
         qseq = before + fixedinv + after
 
-    # next fix translocations
-
     # write out temp fasta
     fixedrecord.seq = qseq
     SeqIO.write(fixedrecord, args.tfasta, 'fasta')
+
+    # Find inversions in terms of Reference position, and output them in VCF format
+    # chromname = 'snps_indels|NC_001416.1|'
+    chromname = qrecord.id
+    # parse args.rdiff and make args.vcf with just the inversions, translocations, etc. in vcf format (no header)
+    # which biodiff will eventually combine with its own vcf from the other differences it finds
+    vcflines = []
+    with open(args.rdiff) as rfile:
+        line2inv = False  # dnadiff records inversions in 2 lines. first occurrence is start, 2nd is end.
+        invstart = 0
+        for line in rfile:
+            cleanline = line.strip('\n')
+            rdiff = cleanline.split('\t')
+            # rdiff[2] is genome position, rdiff[1] is variation type (GAP, INV, etc)
+            if rdiff[1] == 'INV' and not line2inv:
+                vcfstring = chromname + '\t' + rdiff[2] + '\t.\tN\tINV\t.\tPASS\t'
+                line2inv = True
+                invstart = int(rdiff[2])
+            elif rdiff[1] == 'INV':
+                vcfstring += 'SVLEN=' + str(int(rdiff[2]) - invstart) + ';SVTYPE=INV\n'
+                vcflines.append(vcfstring)
+                line2inv = False
+    with open(args.vcf, 'w') as vfile:
+        for line in vcflines:
+            vfile.write(line)
 
 if __name__ == '__main__':
     main()
